cfdbcd00a1359e19210144a7f917dde419028ce3
anthony vouin <avouin@normandiewebschool.fr>
2024-06-07 11:37:52 +0200

    add dotenv

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,5 +1,9 @@
 // config/db.config.js
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Pour charger les variables depuis mon .env
+dotenv.config();
 
 // Clé secrète pour JWT
 export const secretKey:string = 'votre_clé_secrète';
@@ -7,10 +11,12 @@ export const secretKey:string = 'votre_clé_secrète';
 //Connexion à la base de données 
 const connectDB = async () => {
   try {
-    const uri:string ="mongodb+srv://avouin:8HMchxkcNPeqSpsR@portfolio.xhlxukc.mongodb.net/?retryWrites=true&w=majority&appName=portfolio";
+    const uri:string | undefined  =process.env.MONGO_URI ;
 
+    if (uri){ 
     await mongoose.connect(uri);
-    console.log('Connexion à la base de données établie');
+    console.log('Connexion à la base de données établie'); 
+  }
   } catch (error:any) {
     console.error('Erreur de connexion à la base de données:', error.message);
     process.exit(1);
