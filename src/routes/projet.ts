5712efc1d10f0a41df287c31bf274a48106d8459
anthony vouin <avouin@normandiewebschool.fr>
2024-06-10 15:03:22 +0200

    add middleware validedataprojet

--- src/routes/projet.ts
+++ src/routes/projet.ts
@@ -3,12 +3,14 @@ import { createProjet}  from "../controllers/projet.controller";
 import upload from "../../config/multer.config";
 import { jwtMiddleware } from "../middleware/jwtMiddleware";
 import { adminMiddleware } from "../middleware/adminMiddleware";
+import { validateProjetData } from "../middleware/validateProjetDataMiddleware";
+
 
 const router: Router = express.Router();
 
 
 
-router.post("/",  upload.single('image'), jwtMiddleware, adminMiddleware, createProjet);
+router.post("/",  upload.single('image'),  validateProjetData,jwtMiddleware, adminMiddleware, createProjet);
 
 
 export default router;
